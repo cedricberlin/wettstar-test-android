ac077a96d91fd0ec68d50dfd5a767683a393d8a3
Cedric+1-at-121677587242 <cb@cberlin-it.de>
2023-01-20 16:30:05 +0100

    First Commit

---
+++ app/src/main/java/de/wettstartest/MainActivity.java
@@ -0,0 +1,20 @@
+package de.wettstartest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.wettstartest.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/de/wettstartest/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package de.wettstartest.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
